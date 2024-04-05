97d690fab70af679c5cd3866dedc869faa16cd84
Kevin Van Leer <kvl@kvlllc.com>
2024-04-05 09:29:43 -0500

    Added issuer test cases

--- tapi/issuers/_.test.js
+++ tapi/issuers/_.test.js
@@ -41,7 +41,42 @@ describe('issuers', () => {
     createdIssuerId = data.issuerDetails[1][0].issuerId;
     expect(createdIssuerId).toMatch(/[0-9]+/);
   });
-  it('updateIssuer -- valid', async () => {
+  it('updateIssuer -- invalid issuer ID', async () => {
+    const { data } = await updateIssuer({
+      issuerId: 'invalid-issuer-id',
+      issuerStatus: 'Approved',
+      field1: 'open',
+    });
+    expect(data).toStrictEqual({
+      statusCode: '136',
+      statusDesc: 'Issuer Account does not exist.',
+    });
+  });
+  it('updateIssuer -- arbitrary value', async () => {
+    expect(createdIssuerId).toMatch(/[0-9]+/);
+    const updatedIssuer = {
+      issuerId: createdIssuerId,
+      issuerStatus: 'ArbitraryValue',
+      field1: 'open',
+    };
+    const { data } = await updateIssuer(updatedIssuer);
+    expect(data).toStrictEqual(
+      expect.objectContaining({
+        statusCode: '101',
+        statusDesc: 'Ok',
+        issuerDetails: expect.arrayContaining([
+          true,
+          expect.arrayContaining([
+            {
+              issuerId: createdIssuerId,
+              issuerStatus: 'ArbitraryValue',
+            },
+          ]),
+        ]),
+      }),
+    );
+  });
+  it('updateIssuer -- approved', async () => {
     expect(createdIssuerId).toMatch(/[0-9]+/);
     const updatedIssuer = {
       issuerId: createdIssuerId,
@@ -57,13 +92,36 @@ describe('issuers', () => {
           true,
           expect.arrayContaining([
             {
-              issuerId: expect.anything(),
+              issuerId: createdIssuerId,
               issuerStatus: 'Approved',
             },
           ]),
         ]),
       }),
     );
+  });
+  it('getIssuer -- no ID', async () => {
+    const { data } = await getIssuer();
+    expect(data).toStrictEqual({
+      'Error(s)': 'issuerId parameter missing',
+      statusCode: '106',
+      statusDesc: 'Data/parameter missing',
+    });
+  });
+  it('getIssuer -- invalid ID nonnumeric', async () => {
+    const { data } = await getIssuer('invalid-issuer-id');
+    expect(data).toStrictEqual({
+      'Error(s)': 'issuerId : u0026nbsp;Numeric values only',
+      statusCode: '106',
+      statusDesc: 'Data/parameter missing',
+    });
+  });
+  it('getIssuer -- invalid ID numeric', async () => {
+    const { data } = await getIssuer(123.4123);
+    expect(data).toStrictEqual({
+      statusCode: '136',
+      statusDesc: 'Issuer Account does not exist.',
+    });
   });
   it('getIssuer -- success', async () => {
     expect(createdIssuerId).toMatch(/[0-9]+/);
@@ -81,16 +139,6 @@ describe('issuers', () => {
       }),
     );
   });
-  it('deleteIssuer -- success', async () => {
-    expect(createdIssuerId).toMatch(/[0-9]+/);
-    const { data } = await deleteIssuer(createdIssuerId);
-    expect(data).toStrictEqual(
-      expect.objectContaining({
-        statusCode: '101',
-        statusDesc: 'Issuer deleted successfully!',
-      }),
-    );
-  });
   it('getAllIssuers', async () => {
     const { data } = await getAllIssuers();
     expect(data).toStrictEqual(
@@ -102,6 +150,39 @@ describe('issuers', () => {
             issuerId: expect.anything(),
           }),
         ]),
+      }),
+    );
+  });
+  it('deleteIssuer -- no ID', async () => {
+    const { data } = await deleteIssuer();
+    expect(data).toStrictEqual({
+      'Error(s)': 'issuerId parameter missing',
+      statusCode: '106',
+      statusDesc: 'Data/parameter missing',
+    });
+  });
+  it('deleteIssuer -- invalid ID nonnumeric', async () => {
+    const { data } = await deleteIssuer('invalid-issuer-id');
+    expect(data).toStrictEqual({
+      'Error(s)': 'issuerId : u0026nbsp;Numeric values only',
+      statusCode: '106',
+      statusDesc: 'Data/parameter missing',
+    });
+  });
+  it('deleteIssuer -- invalid ID numeric', async () => {
+    const { data } = await deleteIssuer(123.4123);
+    expect(data).toStrictEqual({
+      statusCode: '136',
+      statusDesc: 'Issuer Account does not exist.',
+    });
+  });
+  it('deleteIssuer -- success', async () => {
+    expect(createdIssuerId).toMatch(/[0-9]+/);
+    const { data } = await deleteIssuer(createdIssuerId);
+    expect(data).toStrictEqual(
+      expect.objectContaining({
+        statusCode: '101',
+        statusDesc: 'Issuer deleted successfully!',
       }),
     );
   });
